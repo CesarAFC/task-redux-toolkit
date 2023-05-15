7a90dd7dfa303bd8f1c82816b20b027669c8f25f
Cesar Fontalvo <cafc2030@gmail.com>
2023-05-15 11:40:16 -0500

    Adding a middleware to redux

--- src/app/store.js
+++ src/app/store.js
@@ -2,13 +2,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import tasksReducer from '../features/task/taskSlice';
 import darkReducer from '../features/task/darmodeSlice';
 import storage from 'redux-persist/lib/storage';
-import { persistReducer } from 'redux-persist';
+import { persistReducer, FLUSH, REHYDRATE, REGISTER, PURGE, PAUSE, PERSIST } from 'redux-persist';
 import { combineReducers } from '@reduxjs/toolkit';
+import { getDefaultMiddleware } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 
 const persistConfig = {
     key: 'root',
     storage,
-    whitelist: 'tasksReducer'
+    whitelist: 'tasksReducer',
+    // stateReconcilier: autoMergeLevel2
 }
 
 const reducer = combineReducers({
@@ -19,4 +22,10 @@ const reducer = combineReducers({
 const persistedReducer = persistReducer(persistConfig, reducer)
 export const store = configureStore({
     reducer: persistedReducer,
+    // middleware: (getDefaultMiddleware) => getDefaultMiddleware({thunk: true,
+    // serializableCheck: {
+    //     ignoreActions: [FLUSH, REHYDRATE, REGISTER, PURGE, PAUSE, PERSIST]
+    // }
+    // }).concat()
+    middleware: [thunk]
 })
